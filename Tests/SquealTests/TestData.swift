7e6fbd99f51a34ef5ce028df7e83382e7588b7ff
Sacha DSO <sachadso@gmail.com>
2025-01-15 10:45:35 -1000

    Update TestData.swift

--- Tests/SquealTests/TestData.swift
+++ Tests/SquealTests/TestData.swift
@@ -8,42 +8,160 @@
 import Foundation
 @testable import Squeal
 
+
+// How I would like to write it
+//struct People: Table {
+//    static let name = "people"
+//    var tableName: String = "people"
+//    let firstname = Field<String>(name: "first_name")
+//    let lastname = Field<String>(name: "last_name")
+//}
+
+
 struct Person {
     let firstname: String
     let lastname: String
 }
 
-struct UsersTable: Table {
-    let tableName = "users"
-    let uuid = Field<UUID>(name: "uuid")
-    let id = Field<Int>(name: "id")
-    let name = Field<String>(name: "name")
+
+struct InternalType<T> {
+    
 }
 
-struct PersonTable: Table {
-    var tableName: String = "people"
-    let firstname = Field<String>(name: "first_name")
-    let lastname = Field<String>(name: "last_name")
+
+//enum Columns: InternalType<Any> {
+//    typealias RawValue = <#type#>
+//    
+//    case uuid
+//    case id
+//    case name
+//}
+
+
+
+//struct UsersTable: Table {
+//    
+//    enum Columns: String, CaseIterable {
+//        case uuid
+//        case id
+//        case name
+//    }
+//    
+//
+//    let tableName = "users"
+//    
+////    @Column<UUID>(key: UsersColumn.uuid)
+////    var uuid
+////    
+////    @Column<Int>(key: UsersColumn.id)
+////    var id
+////    
+////    @Column<String>(key: UsersColumn.name)
+////    var name
+//    
+//    let uuid = Field<UUID>(name: Columns.uuid)
+//    let id = Field<Int>(name: "id")
+//    let name = Field<String>(name: "name")
+//}
+
+//struct UsersTable: Table {
+//    
+//    static let name = "users"
+//    let tableName = "users"
+//    
+//    enum Columns: String, CaseIterable {
+//        case uuid
+//        case id
+//        case name
+//    }
+//    
+//    let uuid = Field<UUID>(Columns.uuid)
+//    let id = Field<Int>(Columns.id)
+//    let name = Field<String>(Columns.name)
+//}
+
+
+struct UsersTable: Table {
+    
+    static let schema = "users"
+//    let tableName = "users"
+    
+    enum Columns: String, CaseIterable {
+        case uuid
+        case id
+        case name
+    }
+    
+    @Column<UUID>(name: "uuid") var uuid
+    @Column<Int>(name: "id") var id
+    @Column<String>(name: "name") var name
 }
 
 struct TradesTable: Table {
-    let tableName = "trades"
-    let user_id = Field<UUID>(name: "user_id")
-    let study_id = Field<UUID>(name: "study_id")
-    let type = Field<String>(name: "type")
+    static let schema = "trades"    
+    @Column<UUID>(name: "user_id") var user_id
+    @Column<UUID>(name: "study_id") var study_id
+    @Column<String>(name: "type") var type
 }
 
+
+//@propertyWrapper struct Column<T> {
+//    let key: any RawRepresentable<String>
+//
+//    var wrappedValue: Field<T> {
+//        get { Field<T>(name: key.rawValue) }
+////        set { storage.setValue(newValue, forKey: key) }
+//    }
+//}
+
+//struct UserzTable: Table {
+//    
+//    
+//    let tableName = "users"
+//    
+//    @Column<UUID>(key: "uuid")
+//    var uuid
+//    
+//    @Column<Int>(key: "id")
+//    var id
+//    
+//    // = Field<UUID>(name: "uuid")
+////    let id = Field<Int>(name: "id")
+////    let name = Field<String>(name: "name")
+//}
+
+
+struct PersonTable: Table {
+    static let schema = "people"
+//    var tableName: String = "people"
+    
+    @Column<String>(name: "first_name")
+    var firstname
+    
+    @Column<String>(name: "last_name")
+    var lastname
+}
+//
+//struct TradesTable: Table {
+//    static let name = "trades"
+//    let tableName = "trades"
+//    let user_id = Field<UUID>(name: "user_id")
+//    let study_id = Field<UUID>(name: "study_id")
+//    let type = Field<String>(name: "type")
+//}
+//
 struct StudiesTable: Table {
-    let tableName = "studies"
+    static let schema = "studies"
+//    let tableName = "studies"
         
-    let id = Field<UUID>(name: "id")
-    let prolific_study_id = Field<String?>(name: "prolific_study_id")
-    let name = Field<String>(name: "name")
-    let starting_cash = Field<Double>(name: "starting_cash")
-    let partitioning = Field<Double>(name: "partitioning")
-    let completion_link = Field<String?>(name: "completion_link")
-    let shows_results = Field<Bool>(name: "shows_results")
-    let allows_fractional_investing = Field<Bool>(name: "allows_fractional_investing")
+    @Column<UUID>(name: "id") var id
+    @Column<String?>(name: "prolific_study_id") var prolific_study_id
+    @Column<String>(name: "name") var name
+    @Column<Double>(name: "starting_cash") var starting_cash
+    @Column<Double>(name: "partitioning") var partitioning
+    @Column<String?>(name: "completion_link") var completion_link
+    @Column<Bool>(name: "shows_results") var shows_results
+    @Column<Bool>(name: "allows_fractional_investing") var allows_fractional_investing
 }
 
 // Rename SQLQuery to just SQL
@@ -71,3 +189,26 @@ struct Study {
 // Order By / ASC DESC NULL NOT NULL
 // Having
 // UPDATE SET
+
+
+// JOIN, HAVING, DISTINcT, BETWEEN,  EXISTS NOT EXISTS
+
+// SELECT AS.
+
+
+
+
+
+
+
+
+
+struct OrdersTable: Table {
+    
+    static let schema = "orders"
+    
+    
+    @Column<UUID>(name: "user_id")
+    var user_id
+}
+
